616d1622d36ccba4d0b2445c66f7cefbc9909c96
Ricky Ghrist <rickyghristportfolio@gmail.com>
2023-03-21 23:05:39 -0400

    make map for level 1 child

--- src/coolComponent.js
+++ src/coolComponent.js
@@ -2,15 +2,13 @@ import { CoolComponentChild } from "./coolComponentChild";
 
 export const CoolComponent = () => {
     console.log("rerendering coolComponent.js")
+    const numbers = [1, 2, 3, 4, 5];
     return (
         <div>
 
-            <CoolComponentChild />
-            <CoolComponentChild />
-            <CoolComponentChild />
-            <CoolComponentChild />
-            <CoolComponentChild />
-            <CoolComponentChild />
+            {numbers.map(() =>
+                    <CoolComponentChild />
+            )}
 
         </div>
     );
